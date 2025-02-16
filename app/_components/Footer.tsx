1c37548294e981fe2b2b328460c18dff3f7d92c6
Hermann Richy <hermannrichy15@gmail.com>
2025-02-16 11:44:30 +0100

    Ajustement du code

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,11 +1,12 @@
 import { Section } from "./Section";
+import { Code } from "./Code";
 
 export const Footer = () => {
     return (
         <div>
             <Section className="text-center my-12 py-10">
                 <p className="text-muted-foreground">
-                    © 2024 Hermann Richy. Tous droits réservés.
+                    © 2024 <Code>Hermann Richy</Code>. Tous droits réservés.
                 </p>
             </Section>
         </div>
